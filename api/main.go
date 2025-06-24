b62f12c72b51582206bac2b48ec4ded364626d72
BabyBird <bb@nabyboot.com>
2025-06-24 01:51:24 -0700

    ok logs are meaningful now

--- api/main.go
+++ api/main.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"fmt"
 	"io"
-	"log"
 	"net/http"
 	"os"
 
@@ -16,30 +15,27 @@ import (
 	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/rs/zerolog/log"
 )
 
-// Add to top of main.go
 var (
-	mongoURI    = os.Getenv("MONGO_URI")              // e.g. mongodb://root:example@mongo:27017
-	s3Endpoint  = os.Getenv("S3_ENDPOINT")            // e.g. http://localstack:4566
-	s3Region    = os.Getenv("S3_REGION")              // e.g. us-east-1
-	s3Key       = os.Getenv("AWS_ACCESS_KEY_ID")      // e.g. test
-	s3Secret    = os.Getenv("AWS_SECRET_ACCESS_KEY")  // e.g. test
-	s3Bucket    = os.Getenv("S3_BUCKET")              // e.g. my-mp3-bucket
+	mongoURI   = os.Getenv("MONGO_URI")
+	s3Endpoint = os.Getenv("S3_ENDPOINT")
+	s3Region   = os.Getenv("S3_REGION")
+	s3Key      = os.Getenv("AWS_ACCESS_KEY_ID")
+	s3Secret   = os.Getenv("AWS_SECRET_ACCESS_KEY")
+	s3Bucket   = os.Getenv("S3_BUCKET")
 )
 
 var mongoClient *mongo.Client
 var s3Client *s3.S3
 
-
 func withCORS(h http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		// Set CORS headers
-		w.Header().Set("Access-Control-Allow-Origin", "*") // or restrict to specific origin
+		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 
-		// Handle preflight
 		if r.Method == "OPTIONS" {
 			w.WriteHeader(http.StatusNoContent)
 			return
@@ -49,24 +45,24 @@ func withCORS(h http.HandlerFunc) http.HandlerFunc {
 	}
 }
 
-
 func uploadHandler(w http.ResponseWriter, r *http.Request) {
-	log.Println("Received upload request")
-	r.ParseMultipartForm(10 << 20) // 10MB limit
+	log.Info().Msg("Received upload request")
+	r.ParseMultipartForm(10 << 20)
 
-	log.Println("Parsing form data")
+	log.Info().Msg("Parsing form data")
 	file, handler, err := r.FormFile("file")
 	if err != nil {
-		println("Error retrieving file:", err)
+		log.Error().Err(err).Msg("Error retrieving file")
 		http.Error(w, "Invalid file", 400)
 		return
 	}
-
-	log.Printf("Uploaded file: %s, Size: %d bytes\n", handler.Filename, handler.Size)
 	defer file.Close()
+
+	log.Info().Str("filename", handler.Filename).Int64("size", handler.Size).Msg("Uploaded file")
 
 	tempFile, err := os.CreateTemp("", "upload-*.mp3")
 	if err != nil {
+		log.Error().Err(err).Msg("Error creating temp file")
 		http.Error(w, "Can't create temp file", 500)
 		return
 	}
@@ -75,25 +71,24 @@ func uploadHandler(w http.ResponseWriter, r *http.Request) {
 	io.Copy(tempFile, file)
 	tempFile.Seek(0, 0)
 
-	bucket := "my-mp3-bucket"
-	key := handler.Filename
-
 	_, err = s3Client.PutObject(&s3.PutObjectInput{
-		Bucket: aws.String(bucket),
-		Key:    aws.String(key),
+		Bucket: aws.String(s3Bucket),
+		Key:    aws.String(handler.Filename),
 		Body:   tempFile,
 	})
 	if err != nil {
+		log.Error().Err(err).Msg("Failed to upload to S3")
 		http.Error(w, "Failed to upload to S3", 500)
 		return
 	}
 
 	coll := mongoClient.Database("audio").Collection("uploads")
 	_, err = coll.InsertOne(context.Background(), bson.M{
-		"filename": key,
-		"s3_url":   fmt.Sprintf("s3://%s/%s", bucket, key),
+		"filename": handler.Filename,
+		"s3_url":   fmt.Sprintf("s3://%s/%s", s3Bucket, handler.Filename),
 	})
 	if err != nil {
+		log.Error().Err(err).Msg("Mongo insert failed")
 		http.Error(w, "Mongo insert failed", 500)
 		return
 	}
@@ -103,23 +98,22 @@ func uploadHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	log.Println("Starting server...")
+	log.Info().Msg("Starting server...")
+
 	var err error
 	mongoClient, err = mongo.Connect(context.Background(), options.Client().ApplyURI(mongoURI))
 	if err != nil {
-		log.Println("Failed to connect to MongoDB:", err)
-		log.Fatal(err)
+		log.Fatal().Err(err).Msg("Failed to connect to MongoDB")
 	}
+	log.Info().Msg("Connected to MongoDB")
 
-	log.Println("Connected to MongoDB")
 	sess, err := session.NewSession(&aws.Config{
 		Region:      aws.String(s3Region),
 		Endpoint:    aws.String(s3Endpoint),
 		Credentials: credentials.NewStaticCredentials(s3Key, s3Secret, ""),
 	})
-
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal().Err(err).Msg("Failed to create AWS session")
 	}
 	s3Client = s3.New(sess)
 
@@ -128,6 +122,7 @@ func main() {
 		fmt.Fprint(w, "Welcome to the MP3 Upload Service!")
 	}))
 
+	log.Info().Msg("Server listening on :8080")
 	http.ListenAndServe(":8080", nil)
 }
 
